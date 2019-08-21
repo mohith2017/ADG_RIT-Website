68901d9c73bac8653d897c919095f321104779f6
Rakshith <Rakshith.rex.r@gmail.com>
2019-08-21 12:35:40 +0530

    ready for dash

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+
+import { Route, Switch } from 'react-router-dom';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -5,7 +7,7 @@ import Example from './components/beta';
 
 export const style={
     width:'100vw',
-    height: '120vh',
+    height: '100vh',
     overflow: 'auto',
     backgroundPosition: 'center',
     backgroundColor: 'black',
@@ -16,16 +18,22 @@ export const style={
 class App extends Component {
   render() {
     return (
-      <div className="App1" style={style}>
-
-        <Example />
-        {/* <img src='https://yt3.ggpht.com/a/AGF-l7-wxI7DLGTXjXY0-SYuB7Os-aFHwgmtf4jPUQ=s900-c-k-c0xffffffff-no-rj-mo'/> */}
-        {/* <header className="App-header">
-          //     <img src={logo} className="App-logo" alt="logo" />
-          //     <h1 className="App-title">React Express Starter</h1>
-          //   </header>
-        //   <Customers /> */}
-      </div>
+      <Switch>
+        <div className="App1" style={style}>
+          <Switch>
+            <Route path='/' component={Example} />
+            <Route path='/hello' Component={Example} />
+            {/*  change to dashboard components*/}
+            <Route path='/dashboard' Component={Example} />
+            </Switch>
+          {/* <img src='https://yt3.ggpht.com/a/AGF-l7-wxI7DLGTXjXY0-SYuB7Os-aFHwgmtf4jPUQ=s900-c-k-c0xffffffff-no-rj-mo'/> */}
+          {/* <header className="App-header">
+            //     <img src={logo} className="App-logo" alt="logo" />
+            //     <h1 className="App-title">React Express Starter</h1>
+            //   </header>
+          //   <Customers /> */}
+        </div>
+      </Switch>
     );
   }
 }
